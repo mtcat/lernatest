c57d90ce58d3ba9fe3f74e883ba5c0cb7fb3d5c1
miaoting <miaoting@supos.com>
2023-02-09 16:01:09 +0800

    新增package @mt/dev

--- packages/miaoting_app/index.js
+++ packages/miaoting_app/index.js
@@ -2,8 +2,10 @@ const moment = require('moment');
 const _ = require('lodash');
 const plus = require('miaoting_plus');
 const sub = require('miaoting_sub');
+const dev = require('@mt/dev');
 
 console.log('t', moment());
 console.log('_', _.max([1, 9]));
 console.log('plus', plus(2, 3));
 console.log('sub', sub(2, 3));
+console.log('dev', dev(10, 2));
---
+++ packages/mt_dev/index.js
@@ -0,0 +1,5 @@
+function dev(a, b) {
+  return a / b;
+}
+
+module.exports = dev;
